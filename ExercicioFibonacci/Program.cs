b92ff8d81a123d2a53dbe4d66f65c69059fa2ba3
gabsoares <soaresgabriel68@gmail.com>
2024-04-23 09:38:30 -0300

    Feature - nova lógica para imprimir a partir do 0

--- ExercicioFibonacci/Program.cs
+++ ExercicioFibonacci/Program.cs
@@ -1,17 +1,15 @@
-﻿int fibonacci, numero = 0, anterior = 1, proximo, atual = 0;
-
+﻿int fibonacci, numero = 0, anterior = 1, proximo, atual = -1;
 
 while (numero < 1)
 {
     Console.Write("Digite até qual termo da sequência de fibonacci deseja ver (digite número 1 ou maior): ");
     numero = int.Parse(Console.ReadLine());
-    Console.WriteLine("Fibonnaci: "+0);
-    for (int i = 2; i <= numero; i++)
+    for (int i = 0; i < numero; i++)
     {
         proximo = anterior + atual;
         anterior = atual;
         atual = proximo;
-        Console.WriteLine($"Fibonnaci: {atual}");
+        Console.WriteLine($"Fibonnaci: {atual * -1}");
     }
 }
 
